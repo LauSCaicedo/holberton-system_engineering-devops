ba213cb2db13d4a40ce2064d8ebcc53f068b636f
LauSCaicedo <lauscaicedo@gmail.com>
2022-02-15 21:07:33 -0800

    Update pycodestyle

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,9 +8,12 @@ import requests
 
 
 def top_ten(subreddit):
-    """ Write a function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
+    """ Write a function that queries the Reddit API
+    and prints the titles of the first 10 hot posts
+    listed for a given subreddit.
     """
-    base_url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    base_url = 'https://www.reddit.com/r/{}/hot.json?limit=10'\
+        .format(subreddit)
     headers = {'User-Agent': 'Too Many Requests'}
     response = requests.get(base_url, headers=headers)
 
