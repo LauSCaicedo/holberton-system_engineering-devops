27dba9077af481ec187f75d14d52628829f1400d
LauSCaicedo <3383@holbertonschool.com>
2022-02-17 22:43:41 -0500

    Update file 0-strace..

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,3 +1,7 @@
+# Using strace, find out why Apache is returning a 500 error.
+# Once you find the issue, fix it and then automate it using Puppet 
+# (instead of using Bash as you were previously doing).
+
 exec {'Replace':
     path    => '/usr/bin:/usr/sbin:/bin',
     command => 'sed -i 's/phpp/php/g' /var/www/html/wp-settings.php',
